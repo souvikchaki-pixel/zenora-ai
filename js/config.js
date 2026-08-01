2235147ecae257163a13cc1e17e8ffbb27711718
souvikchaki-pixel <souvik2563chaki@gmail.com>
2026-08-01 22:20:23 +0530

    config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 window.ZENORA_CONFIG = {
   // Supabase Configuration
   SUPABASE_URL: "https://zqcdtixifoowdfhxdsbnu.supabase.co",
-  SUPABASE_ANON_KEY: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxY2R0aXhpZm93ZGZoeGRzYm51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzODY5ODgsImV4cCI6MjEwMDk2Mjk4OH0.fExZ3X0BXqRC-scHcTG6B1sqUVAZZQP3IrIeHaMdkyI",
   
   // Groq Model Setting (Key is securely handled via Vercel Environment Variables)
   GROQ_MODEL: "llama-3.3-70b-versatile"
